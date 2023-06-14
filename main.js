1475abdb4eca59f17ea9aeef59fc5acb63878a83
Jasir Limani <jasirlimani12@gmail.com>
2023-06-15 00:25:17 +0200

    made progress

--- main.js
+++ main.js
@@ -1,35 +1,76 @@
-let movieBtn = document.getElementById('moviesButton')
-let seriesBtn = document.getElementById('SeriesButton')
-let moviesCont = document.getElementById('movies')
-let seriesCont = document.getElementById('series')
+
+let movieBtn = document.getElementById('moviesButton');
+let seriesBtn = document.getElementById('SeriesButton');
+let netflixMoviesCont = document.getElementById('netflixMovies');
+let netflixSeriesCont = document.getElementById('netflixSeries');
+let huluMoviesCont = document.getElementById('huluMovies');
+let huluSeriesCont = document.getElementById('huluSeries');
 
 movieBtn.addEventListener('click', function() {
     anime({
-        targets: '#series',
+        targets: ['#netflixSeries', '#huluSeries'],
         opacity: 0
-    })
+    });
     anime({
-        targets: '#movies',
+        targets: ['#netflixMovies',  '#huluMovies'],
         opacity: 1
-    })
-    showMovies()
-})
+    });
+    showMovies();
+});
+
 seriesBtn.addEventListener('click', function() {
     anime({
-        targets: '#movies',
+        targets: ['#netflixMovies', '#huluMovies'],
         opacity: 0
-    })
+    });
     anime({
-        targets: '#series',
+        targets: ['#netflixSeries', '#huluSeries'],
         opacity: 1
-    })
-    showSeries()
-})
-function showMovies(){
-    moviesCont.style.display = 'block'
-    seriesCont.style.display = 'none'
+    });
+    showSeries();
+});
+
+function showMovies() {
+    netflixMoviesCont.style.display = 'block';
+    netflixSeriesCont.style.display = 'none';
+    huluMoviesCont.style.display = 'block';
+    huluSeriesCont.style.display = 'none';
 }
-function showSeries(){
-    seriesCont.style.display = 'block'
-    moviesCont.style.display = 'none'
+
+function showSeries() {
+    netflixMoviesCont.style.display = 'none';
+    netflixSeriesCont.style.display = 'block';
+    huluMoviesCont.style.display = 'none';
+    huluSeriesCont.style.display = 'block';
 }
+
+let netflixBtn = document.getElementById('netflixButton');
+let huluBtn = document.getElementById('huluButton');
+let netflixCont = document.getElementById('netflix');
+let huluCont = document.getElementById('hulu');
+
+netflixBtn.addEventListener('click', function() {
+    anime({
+        targets: '#hulu',
+        opacity: 0
+    });
+    anime({
+        targets: '#netflix',
+        opacity: 1
+    });
+    netflixCont.style.display = 'block';
+    huluCont.style.display = 'none';
+});
+
+huluBtn.addEventListener('click', function() {
+    anime({
+        targets: '#netflix',
+        opacity: 0
+    });
+    anime({
+        targets: '#hulu',
+        opacity: 1
+    });
+    huluCont.style.display = 'block';
+    netflixCont.style.display = 'none';
+});
